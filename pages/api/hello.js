78e827144f340fba5c30e6a8595e6837e5b92031
Ezeuju <ezeuju1919@gmail.com>
2023-07-10 08:51:15 +0100

    first commit

---
+++ pages/api/hello.js
@@ -0,0 +1,10 @@
+export default function handler(req, res) {
+    res.status(200).json({ text: 'Hello' });
+  }
+
+
+  //server code.
+ // export default function handler(req, res) {
+   // const email = req.body.email;
+    // Then save email to your database, etc...
+  }
